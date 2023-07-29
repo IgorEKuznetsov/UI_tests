ff88b106dfb7c0a2f9ff1ec6567616d3d6e5388b
ikv <igor88@bk.ru>
2023-07-29 19:07:40 +0300

    pipline enabled

--- jenkins/ui-jenkins-pipline.groovy
+++ jenkins/ui-jenkins-pipline.groovy
@@ -1,30 +1,30 @@
 timeout(180) {
     node('maven') {
-        timestamp {
-            wrap([$class: 'BuildUser']) {
-                summary = """|<b>Owner:</b> ${env.BUILD_USER}
-	                            |<b>Branch:</b> ${BRANCH}""".stripMargin()
-                currentBuild.description = summary.replaceAll("\n", "<br>")
-                owner_user_id = "${env.BUILD_USER_ID}"
-            }
-            //yaml_object = readYaml $YAML_CONFIG
-            //for (key in params.keySet()) {
-            //    {
-            //        System.setProperty(key, params[key])
-            //    }
-            //}
-            stage('Checkout') {
-                checkout scv
-            }
-            stage('Run tests') {
-                tests_exit_code = sh(
-                        script: "mvn test -Dbrowser=$BROWSER_NAME -Dbrowser.version=$BROWSER_VERSION",
-                )
 
-                if (tests_exit_code != 0) {
-                    currentBuild.status = 'UNSTABLE'
-                }
+        wrap([$class: 'BuildUser']) {
+            summary = """|<b>Owner:</b> ${env.BUILD_USER}
+	                            |<b>Branch:</b> ${BRANCH}""".stripMargin()
+            currentBuild.description = summary.replaceAll("\n", "<br>")
+            owner_user_id = "${env.BUILD_USER_ID}"
+        }
+        //yaml_object = readYaml $YAML_CONFIG
+        //for (key in params.keySet()) {
+        //    {
+        //        System.setProperty(key, params[key])
+        //    }
+        //}
+        stage('Checkout') {
+            checkout scv
+        }
+        stage('Run tests') {
+            tests_exit_code = sh(
+                    script: "mvn test -Dbrowser=$BROWSER_NAME -Dbrowser.version=$BROWSER_VERSION",
+            )
+
+            if (tests_exit_code != 0) {
+                currentBuild.status = 'UNSTABLE'
             }
         }
+
     }
 }
